d15babf4344a92de5698a4ed528a272022166d04
Nick Garcia <47847297+designi@users.noreply.github.com>
2020-03-21 18:00:45 -0700

    Update CoronaVirusDownloader.py

--- CoronaVirusDownloader.py
+++ CoronaVirusDownloader.py
@@ -8,21 +8,20 @@ from urllib.error import HTTPError
 # COVID-19 cases worldwide
 dt = datetime.today().strftime('%Y-%m-%d')
 url = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-{0}.xlsx'.format(dt)
+fileName = url[url.find('COVID'):len(url)]
 fileLocation = '/Users/nikolasgarcia/PycharmProjects/CoronaVirus/DailyDownload/'
 
 try:
     df = pd.read_excel(url, index_col='DateRep', parse_dates=True)
-    df.index = pd.to_datetime(df.index,format='%Y-%m-%d')
+    df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
     logging.info('Successfully pulled down file')
     print(df.head())
-    if path.exists(fileLocation.format(url[url.find('COVID'):len(url)])):
-        logging.info('Moving {0} file to \n'+fileLocation.format(url[url.find('COVID'):len(url)]))
-        df.to_excel(fileLocation+'{0}'.format(url[url.find('COVID'):len(url)]), sheet_name='Data',Index=True)
+    if path.exists(fileLocation.format(fileName)):
+        logging.info('Moving {0} file to \n'+fileLocation.format(fileName))
+        df.to_excel(fileLocation+'{0}'.format(fileName), sheet_name='Data', index=True)
     else:
         logging.info('File already exists at {0}'.format(fileLocation))
 except FileNotFoundError as e:
     logging.error('Encountered {0} while trying to download. \nCheck URL.'.format(e))
 except HTTPError as e:
     logging.error('Encountered {0} error. Web Link {1}.'.format(e.code, e.reason))
-
-
